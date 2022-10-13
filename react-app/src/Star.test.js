fedd077722d6a4a76b4779e7a4e7f9db60a63778
kamran-hassan <hassan.kamran107@gmail.com>
2022-10-13 15:43:49 +0530

    Testing React App using @testing-library/react

---
+++ react-app/src/Star.test.js
@@ -0,0 +1,8 @@
+import { render } from "@testing-library/react"
+import { Star } from "./Star.js"
+
+test("renders an h1", () => {
+    const { getByText } = render(<Star />);
+    const h1 = getByText(/Cool Star/);
+    expect(h1).toHaveTextContent("Cool Star");
+});
