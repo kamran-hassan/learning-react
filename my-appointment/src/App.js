7257d2b3be10c6ee9eac17ad3251642feba37802
kamran-hassan <hassan.kamran107@gmail.com>
2022-10-17 14:28:12 +0530

    summerized the listing methods

--- my-appointment/src/App.js
+++ my-appointment/src/App.js
@@ -11,6 +11,9 @@ function styleobjret(col) {
 
 function App() {
   //  inline styling can be done with `` symbol to pass valiable which return a tyle object, direct style tag can be used
+
+  var m1 = "{colordata.map(d => (<h1 style={styleobjret(`${d.value}`)} > { d.color }</h1>))}";
+  var m2 = "{colordata.map((d) => {return (<h1 style={styleobjret(`${d.value}`)} > { d.color }</h1>);})}";
   return (
     
 
@@ -20,8 +23,15 @@ function App() {
       </h1>
       <Search />
 
+      <h1> Listing using Methode one <br /> </h1> <h6>{m1}</h6>
       <ul>
         {colordata.map(d => (<h1 style={styleobjret(`${d.value}`)} > { d.color }</h1>))}
+      </ul>
+      <h1> Listing using Methode two <br /> </h1><h6>{m2}</h6>
+      <ul>
+        {colordata.map((d) => {
+          return (<h1 style={styleobjret(`${d.value}`)} > { d.color }</h1>);
+        })}
       </ul>
       
     </div>
